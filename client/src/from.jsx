a68009f36c77758fb6b134cc8fe900176e8456a4
subhajitjanaxyz <subhajitjanaxyz@gmail.com>
2025-04-15 20:17:18 +0530

    add

--- client/src/from.jsx
+++ client/src/from.jsx
@@ -1,71 +1,71 @@
 import { useState } from "react";
 
 
-export const From=()=>{
-const [fromdata,setfromdata]=useState({
-  name:"",
-  email:""
-})
-const handlesubmit=(e)=>{
-e.preventDefault();
-}
-const handleonchaneinpurfrom=(e)=>{
-  const valuex=e.target.value;
-  const namex=e.target.name;
-  console.log(valuex)
+export const From = () => {
+  const [fromdata, setfromdata] = useState({
+    name: "",
+    email: ""
+  })
+  const handlesubmit = (e) => {
+    e.preventDefault();
+  }
+  const handleonchaneinpurfrom = (e) => {
+    const valuex = e.target.value;
+    const namex = e.target.name;
+    console.log(valuex)
 
-  setfromdata((prev)=>({...prev,[namex]:valuex}))
-}
-
-
+    setfromdata((prev) => ({ ...prev, [namex]: valuex }))
+  }
 
 
-    return (
-<>
 
-<form action="">
 
-  
-<div className="wDFSF50 border bg-body-tertiary p-2">
-  <div className="mb-3">
-    <label htmlFor="formGroupExampleInput" className="form-label">
-     NAME
-    </label>
-    <input
-    value={fromdata.name}
-      type="text"
-      className="form-control"
-      id="formGroupExampleInput"
-      placeholder=" NAME"
-      name="name"
-      onChange={handleonchaneinpurfrom}
-    />
-  </div>
-  <div className="mb-3">
-    <label htmlFor="formGroupExampleInput2" className="form-label">
-      EMAIL
-    </label>
-    <input
-      onChange={handleonchaneinpurfrom}
-      value={fromdata.email}
+  return (
+    <>
 
-    name="email"
-      type="text"
-      className="form-control"
-      id="formGroupExampleInput2"
-      placeholder="EMAIL"
-    />
-    <div className="text-center">
-    <button className="btn btn-info mt-3 mx-auto text-white fw-bolder" type="submit" 
-    onClick={handlesubmit}
-    >submit</button>
-    </div>
-  </div>
-  </div>
+      <form action="">
 
-</form>
 
-</>
+        <div className="wDFSF50 border bg-body-tertiary p-2">
+          <div className="mb-3">
+            <label htmlFor="formGroupExampleInput" className="form-label">
+              NAME
+            </label>
+            <input
+              value={fromdata.name}
+              type="text"
+              className="form-control"
+              id="formGroupExampleInput"
+              placeholder=" NAME"
+              name="name"
+              onChange={handleonchaneinpurfrom}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="formGroupExampleInput2" className="form-label">
+              EMAIL
+            </label>
+            <input
+              onChange={handleonchaneinpurfrom}
+              value={fromdata.email}
 
-    )
+              name="email"
+              type="text"
+              className="form-control"
+              id="formGroupExampleInput2"
+              placeholder="EMAIL"
+            />
+            <div className="text-center">
+              <button className="btn btn-info mt-3 mx-auto text-white fw-bolder" type="submit"
+                onClick={handlesubmit}
+              >submit</button>
+            </div>
+          </div>
+        </div>
+
+      </form>
+
+    </>
+
+  )
 }
--- client/src/table.jsx
+++ client/src/table.jsx
@@ -1,63 +1,63 @@
 import { useDispatch, useSelector } from "react-redux";
-import {datainsert} from "./store/alldata";
+import { datainsert } from "./store/alldata";
 import { useGetnjspostQuery } from "./store/apirtk";
 // import { useEffect } from "react";
 
 
-export const Table=()=>{
+export const Table = () => {
 
-const dispatch=useDispatch()
-const dataapi=useSelector(state=>state.dbdata);
+  const dispatch = useDispatch()
+  const dataapi = useSelector(state => state.dbdata);
 
   //call api
-  const {data,isLoading}=useGetnjspostQuery("67fcbc25a604384fa7d10511");
+  const { data, isLoading } = useGetnjspostQuery("67fcbc25a604384fa7d10511");
   // insert data into variable
-  if(!isLoading){
+  if (!isLoading) {
     dispatch(datainsert(data));
   }
   // when data is avalable render 
-  if(dataapi){
-  console.log("all data from api");
-  console.log(dataapi);
-}
-
-  
-
-
-
-return (
-<>
-<table className="table table-bordered">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">NAME</th>
-      <th scope="col">EMAIL</th>
-      <th scope="col">BUTTON</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>
-
-        <button className="btn btn-primary">edit</button>
-        <button className="btn btn-danger ms-2">delete</button>
-      </td>
-    </tr>
-   </tbody>
-</table>
+  if (dataapi) {
+    console.log("all data from api");
+    console.log(dataapi);
+  }
 
 
 
 
 
-</>
+  return (
+    <>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">NAME</th>
+            <th scope="col">EMAIL</th>
+            <th scope="col">BUTTON</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>
+
+              <button className="btn btn-primary">edit</button>
+              <button className="btn btn-danger ms-2">delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
 
 
 
 
-    )
+
+    </>
+
+
+
+
+  )
 }
